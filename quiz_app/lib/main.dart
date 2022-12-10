658d4ba6c4f3d3f61c42e4090b7548f362c9c778
Arjun Bhandari <er.arjunbhandari@gmail.com>
2022-12-10 21:59:54 +0545

    added quiz and result

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/answer.dart';
-import 'question.dart';
+import 'package:quiz_app/result.dart';
+import 'quiz.dart';
 
 //void main() {
 //  runApp(MyApp());
@@ -16,6 +16,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'what\'s your favourite color?',
+      'answers': [
+        {'text': 'red', 'score': 10},
+        {'text': 'blue', 'score': 7},
+        {'text': 'black', 'score': 5},
+        {'text': 'yellow', 'score': 6}
+      ],
+    },
+    {
+      'questionText': 'what\'s your favourite animal?',
+      'answers': [
+        {'text': 'Dog', 'score': 4},
+        {'text': 'Cat', 'score': 7},
+        {'text': 'Lion', 'score': 9},
+        {'text': 'Tiger', 'score': 2}
+      ],
+    },
+    {
+      'questionText': 'what\'s your favourite sports?',
+      'answers': [
+        {'text': 'cricket', 'score': 0},
+        {'text': 'volleyball', 'score': 4},
+        {'text': 'basketball', 'score': 6},
+        {'text': 'football', 'score': 8}
+      ],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -26,34 +56,17 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'what\'s your favourite color?',
-        'answers': ['red', 'blue', 'black', 'yellow'],
-      },
-      {
-        'questionText': 'what\'s your favourite animal?',
-        'answers': ['Dog', 'Cat', 'Lion', 'tiger'],
-      },
-      {
-        'questionText': 'what\'s your favourite sports?',
-        'answers': ['Cricket', 'Volleyball', 'basketball', 'football'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Quiz App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]['questionText'].toString()),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex)
+            : Result(),
       ),
     );
   }
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'answer.dart';
+import 'question.dart';
+
+class Quiz extends StatelessWidget {
+  final VoidCallback answerQuestion;
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  const Quiz(
+      {required this.questions,
+      required this.answerQuestion,
+      required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(questions[questionIndex]['questionText'].toString()),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(answerQuestion, answer['text'] as String);
+        }).toList(),
+      ],
+    );
+  }
+}
