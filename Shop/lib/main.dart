71685547c9787246412c6bfd48d5d0369a23e2d6
Arjun Bhandari <er.arjunbhandari@gmail.com>
2022-12-29 09:18:31 +0545

    Provider Added

--- Shop/lib/main.dart
+++ Shop/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 
+import 'models/products_provider.dart';
+import 'screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-            .copyWith(secondary: Colors.deepOrange),
-        fontFamily: 'Lato',
-      ),
-      home: ProductsOverviewSCreen(),
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange),
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewSCreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
--- Shop/lib/widgets/product_item.dart
+++ Shop/lib/widgets/product_item.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-  const ProductItem(this.id, this.title, this.imageUrl);
-
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          leading: IconButton(
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              Provider.of<Product>(context).toggleFavorite();
+            },
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+          ),
+          trailing: IconButton(
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
         ),
-        leading: IconButton(
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {},
-          icon: const Icon(Icons.favorite),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
-        trailing: IconButton(
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {},
-          icon: const Icon(Icons.shopping_cart),
-        ),
-      ),
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
       ),
     );
   }
